127da2842155385166eabf8e845966449ed83550
anharfhdn <anharfahrudin21@gmail.com>
2022-12-30 15:33:38 +0700

    lupa hapus secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kckc@hbnjgr%3)1y=7@^lwbb_39_1f3i-$8viap3v13@77+08p'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
